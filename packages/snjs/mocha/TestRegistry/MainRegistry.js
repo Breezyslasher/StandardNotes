cc1f16c70e6ccfc5d43c0d4e7af6c65d5fd51104
Antonella Sgarlatta <anto@standardnotes.com>
2026-04-09 15:37:47 -0300

    Revert "chore: split snjs session tests and increase token ttl [snjs docker only] (#3001)"
    This reverts commit 437ebddbd0c0d8a66a8e4e4c573d7cac460a00c9.

--- packages/snjs/mocha/TestRegistry/MainRegistry.js
+++ packages/snjs/mocha/TestRegistry/MainRegistry.js
@@ -1,9 +1,7 @@
 import { BaseTests } from './BaseTests.js'
-import { SessionTests } from './SessionTests.js'
 import { VaultTests } from './VaultTests.js'
 
 export default {
   BaseTests,
-  SessionTests,
   VaultTests,
 }
--- packages/snjs/mocha/TestRegistry/SessionTests.js
+++
@@ -1,8 +0,0 @@
-export const SessionTests = {
-  enabled: true,
-  exclusive: false,
-  files: [
-    'session.test.js',
-    'session-invalidation.test.js',
-  ],
-}
